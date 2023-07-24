1579200c0559d70a37f4fe61562f3ec56647bc56
Ananya Agrawal <114423878+ananyaa30@users.noreply.github.com>
2023-07-24 11:12:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    text.style.marginTop = value * 2.5 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.top = value * 1 + 'px';
+});
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const slides = document.querySelectorAll('.slide');
+const numberOfSlides = slides.length;
+let slideNumber = 0;
+
+nextBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+
+    slideNumber++;
+    
+    if (slideNumber > (numberOfSlides -1 )) {
+        slideNumber=0;
+    }
+    slides[slideNumber].classList.add('active');
+}
+
+prevBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+
+    slideNumber--;
+
+    if (slideNumber < 0) {
+        slideNumber = numberOfSlides -1;
+    }
+    slides[slideNumber].classList.add('active');
+}
+  
